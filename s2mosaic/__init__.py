f5a74195c6a2a4e7eb912fe92b473390dd4be607
Nick <wrignj08@gmail.com>
2025-06-17 15:54:34 +0800

    added uv support

--- s2mosaic/__init__.py
+++ s2mosaic/__init__.py
@@ -1,8 +1,14 @@
 from .__version__ import __version__
-from .coordinator import mosaic
-
 
 __all__ = [
     "mosaic",
     "__version__",
 ]
+
+
+def __getattr__(name):
+    if name == "mosaic":
+        from .coordinator import mosaic
+
+        return mosaic
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
