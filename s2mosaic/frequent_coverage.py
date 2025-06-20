79308a9e0e5a94c97128ce8951bdf22a66c2d7f9
Nick <wrignj08@gmail.com>
2025-06-20 20:21:19 +0800

    Vectorized rasterize

--- s2mosaic/frequent_coverage.py
+++ s2mosaic/frequent_coverage.py
@@ -10,6 +10,9 @@ from pystac.item_collection import ItemCollection
 from rasterio.features import rasterize
 from rasterio.transform import Affine
 from shapely.geometry import Polygon
+from rasterio.enums import MergeAlg
+
+logger = logging.getLogger(__name__)
 
 
 def get_coverage(scenes: List[Item]) -> gpd.GeoDataFrame:
@@ -18,7 +21,7 @@ def get_coverage(scenes: List[Item]) -> gpd.GeoDataFrame:
         if scene.geometry is not None and "coordinates" in scene.geometry:
             extents.append(Polygon(scene.geometry["coordinates"][0]))
 
-    extent_gdf = gpd.GeoDataFrame(geometry=extents, crs="EPSG:4326")  # type: ignore
+    extent_gdf = gpd.GeoDataFrame(geometry=extents, crs="EPSG:4326")
     return extent_gdf
 
 
@@ -27,29 +30,24 @@ def get_raster_coverage(
 ):
     scene_gdf = gpd.GeoDataFrame(
         [scene_bounds], geometry=[scene_bounds], crs="EPSG:4326"
-    ).to_crs(
-        f"EPSG:{local_crs}"
-    )  # type: ignore
+    ).to_crs(f"EPSG:{local_crs}")
+
     coverage_gdf_local = coverage_gdf.to_crs(f"EPSG:{local_crs}")
 
-    coverage_gdf_local["geometry"] = coverage_gdf_local.buffer(0)  # type: ignore
+    coverage_gdf_local["geometry"] = coverage_gdf_local.make_valid()
 
-    extent = scene_gdf.total_bounds  # type: ignore
+    extent = scene_gdf.total_bounds
     x_min, _, _, y_max = extent
 
-    raster = np.zeros((10980, 10980), dtype=np.int16)
-
-    transform = Affine(resolution, 0, x_min, 0, -resolution, y_max)
-
-    for _, coverage_row in coverage_gdf_local.iterrows():  # type: ignore
-        geom = coverage_row.geometry
-        raster += rasterize(
-            [(geom, 1)],
-            out_shape=raster.shape,
-            fill=0,
-            dtype=np.int16,
-            transform=transform,
-        )
+    geoms_with_values = [(geom, 1) for geom in coverage_gdf_local.geometry]
+    raster = rasterize(
+        geoms_with_values,
+        out_shape=(10980, 10980),
+        fill=0,
+        dtype=np.int16,
+        transform=Affine(resolution, 0, x_min, 0, -resolution, y_max),
+        merge_alg=MergeAlg.add,
+    )
     return raster
 
 
@@ -57,7 +55,7 @@ def get_frequent_coverage(
     scene_bounds: Polygon, scenes: ItemCollection, coverage_threshold_pct=0.1
 ) -> np.ndarray:
     scenes_list = list(scenes)
-    logging.info(f"Calculating total coverage for {len(scenes_list)} scenes")
+    logger.info(f"Calculating total coverage for {len(scenes_list)} scenes")
 
     try:
         local_crs = scenes_list[0].properties["proj:epsg"]
@@ -65,20 +63,23 @@ def get_frequent_coverage(
         local_crs = scenes_list[0].properties["proj:code"]
         local_crs = int(local_crs.split(":")[-1])
 
-    logging.info(f"Using local CRS: EPSG:{local_crs}")
+    logger.info(f"Using local CRS: EPSG:{local_crs}")
 
     coverage_gdf = get_coverage(scenes_list)
     raster = get_raster_coverage(scene_bounds, coverage_gdf, local_crs)
-    logging.info(f"Coverage raster shape: {raster.shape}")
+    logger.info(f"Coverage raster shape: {raster.shape}")
 
     max_count = raster.max()
-    logging.info(f"Max coverage count: {max_count}")
-    # any area that is covered by more than 10% of the scenes is considered to be covered
-    dymanic_threshold = max_count * coverage_threshold_pct
-    logging.info(f"Dynamic threshold: {dymanic_threshold}")
-    # threshold the raster to get a mask of the frequent data
-    frequent_data_mask = raster >= dymanic_threshold
-    # expand the mask to include nearby pixels, this grows the no data areas by 4 pixels
+    logger.info(f"Max coverage count: {max_count}")
+
+    # Any area that is covered by more than 10% of the scenes is considered to be covered
+    dynamic_threshold = max_count * coverage_threshold_pct
+    logger.info(f"Dynamic threshold: {dynamic_threshold}")
+
+    # Threshold the raster to get a mask of the frequent data
+    frequent_data_mask = raster >= dynamic_threshold
+
+    # Expand the mask to include nearby pixels, this grows the no data areas by 4 pixels
     frequent_data_mask = ~scipy.ndimage.binary_dilation(
         ~frequent_data_mask, iterations=4
     )
